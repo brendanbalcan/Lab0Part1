121604624d61135e6a56188cea0a7a443bdf09ee
brendanbalcan <ba04balc@siena.edu>
2026-09-14 14:11:43 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,13 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Brendan Balcan
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("Long Island, Dix Hills Diner, Buffalo Chicken Wrap");
     }
 }
